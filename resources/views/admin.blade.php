b97e8ec21c5502e93626e33e065105a8cff30b73
kemala19067 <kemala.19067@mhs.unesa.ac.id>
2021-10-06 19:33:34 +0700

    coba

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('container')
+    <h1>E-Learning: Coding Yuk</h1>
+    <p>ADMINNNNNNNNNNNNN<br> Website untuk belajar coding bagi pemula</p>
+@endsection
---
+++ resources/views/kursus.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('container')
+    <h1>Coding Yuk - Course</h1>
+    <h2>Course</h2>
+@endsection
